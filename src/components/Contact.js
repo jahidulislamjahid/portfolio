3f27d1d117d3ecf3a014afbd248157b930e7b7b3
jahid <info.jahidulislamjahid@gmail.com>
2021-12-11 15:51:23 +0600

    implementation of emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import JotformEmbed from 'react-jotform-embed';
+import emailjs from 'emailjs-com';
 
 import content from '../content';
 import useWindowPosition from '../hook/useWindowPosition';
 
 export default function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ajf7q2h', 'template_wd91gej', e.target, 'user_HRoNB0POq01uSz2iMRTDq')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset();
+  };
   const animated = useWindowPosition('header', 0.6, 4);
   return (
     <div
@@ -19,8 +32,28 @@ export default function Contact() {
         }}
         className="w-full md:w-4/5 md:rounded-xl shadow-2xl flex md:flex-row flex-col-reverse justify-around items-center"
       >
-        <div className="my-5 py-5">
-          <JotformEmbed src="https://form.jotform.com/213344781404452" />
+        <div className="my-2 py-2">
+          <h3 className='text-white'>Leave Your Message In The Mail</h3>
+          <form onSubmit={sendEmail}>
+            <div className='mb-2'>
+              <textarea id="message" rows="4" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave Your Message..." name="message"></textarea>
+            </div>
+            <div class="mb-2">
+              <input type="text" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="subject" required="" name="subject" />
+            </div>
+            <div class="mb-2">
+              <input type="text" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required="" name="name" />
+            </div>
+            <div class="mb-2">
+              <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="E-Mail" required="" name="email" />
+            </div>
+
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium p-2 rounded-lg text-sm w-full sm:w-auto px-5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Submit</button>
+          </form>
+
+          {/* <label for="message" class="text-sm font-medium text-white block mb-2 text-white">Your message</label> */}
+
+          {/* <JotformEmbed src="https://form.jotform.com/213344781404452" /> */}
         </div>
         <div className="font-dosis w-4/5 md:w-2/5 mt-5 transtion duration-2000">
           <h1
